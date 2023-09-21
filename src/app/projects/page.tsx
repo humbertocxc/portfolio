3faadb5b63f29cb3375a5d8a71a7af1bdfb97559
humberto <humberto.gnm98@gmail.com>
2023-09-21 14:39:46 -0300

    projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,12 @@
-export default function About() {
+import ProjectsList from "@/components/Projects"
+import { projects } from "../constants"
+
+export default function Projects() {
   return (
     <div>
-      Projects
+      <h2 className="pt-24 text-pink-500 font-semibold">MY PROJECTS</h2>
+      <p className="pr-8 pt-2 text-lg">Here you'll discover various projects I've crafted using diverse technologies. Each project comes with a detailed explanation, a link to the repository for code access, and a preview version for a firsthand experience.</p>
+      <ProjectsList projects={projects} />
     </div>
   )
 }
